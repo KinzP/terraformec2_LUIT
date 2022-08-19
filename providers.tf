01d76a06c77fb3c9b200131b58da6ce7d8b8afde
Kinsey Parham <kinseyparham@gmail.com>
2022-08-19 18:02:50 +0000

    LUIT EC2 Terraform project

--- providers.tf
+++ providers.tf
@@ -11,14 +11,5 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+  region  = "us-east-1"
 }
